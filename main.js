29aa3187263110c70cf43e6a69918ac8c072e0fe
Fabian Bucher <git@fabianbucher.de>
2021-01-04 15:09:09 +0100

    take timezone into account for todayString function

--- main.js
+++ main.js
@@ -209,7 +209,17 @@ function buildDays(days) {
 }
 
 function todayString() {
-  return new Date().toISOString().split('T')[0];
+  const date = new Date();
+  let day = date.getDate();
+  if (day < 10) {
+    day = '0' + day;
+  }
+  let month = date.getMonth() + 1;
+  if (month < 10) {
+    month = '0' + month;
+  }
+  const year = date.getFullYear();
+  return `${year}-${month}-${day}`;
 }
 
 function startLoading() {
